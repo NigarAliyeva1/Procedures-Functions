3ef9bcd7b1382685a787a39d8511bb581d76a61d
Nigarkhanim Aliyeva <112957859+NigarAliyeva1@users.noreply.github.com>
2023-08-11 18:08:20 +0400

    Create Open-EndedQuestions.sql

---
+++ Open-EndedQuestions.sql
@@ -0,0 +1,8 @@
+
+**Open-Ended Questions:**
+
+--1. **Question 1:** Explain the concept of encapsulation in the context of procedures and functions in Oracle.
+2. **Question 2:** How do you pass values to a procedure as input parameters? Provide an example.
+3. **Question 3:** What is the difference between an input parameter and an output parameter in a procedure?
+4. **Question 4:** Write a PL/SQL function that calculates the factorial of a given number.
+5. **Question 5:** Compare and contrast the usage of functions in SQL queries and PL/SQL blocks. Provide examples for each case.
